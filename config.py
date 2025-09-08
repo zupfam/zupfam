7be34d0fcefdb042e0a9d9077ee768787b586054
hammaad-swe <hammaad.swe@gmail.com>
2025-09-08 17:55:05 +0530

    feat: uv init

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID")
-GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET")
-GOOGLE_REDIRECT_URI = os.getenv("GOOGLE_REDIRECT_URI")
-DATABASE_URL = os.getenv("DATABASE_URL")
--- scheduler.py
+++
@@ -1,7 +0,0 @@
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from review_fetcher import fetch_and_store_reviews
-
-def start_scheduler():
-    scheduler = AsyncIOScheduler()
-    scheduler.add_job(fetch_and_store_reviews, "interval", minutes=2)
-    scheduler.start()
